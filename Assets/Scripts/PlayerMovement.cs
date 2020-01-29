aa755c15cf2ddff6b247ba5aced556ac7fd95a06
Chau Vu <chv004@ucsd.edu>
2020-01-29 08:49:06 -0800

    Basic PlayerMovement Script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 7f;
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+    int horizontal;
+    int vertical;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+       
+
+    }
+
+    private void FixedUpdate()
+    {
+        // Movement
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
