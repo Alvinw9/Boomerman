0ce54b9d05a2991105576738a3b875bd20837457
Alvinw9 <33185325+Alvinw9@users.noreply.github.com>
2020-02-07 16:29:01 -0800

    added self stun

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,12 +12,13 @@ public class PlayerMovement : MonoBehaviour
     private int numBombs = 1;
     private int bombRange = 1;
     private int explosionDelay = 3;
-    public float speed = 5f;
+    private float speed = 5f;
     private bool bombPenetrate = false;
     private bool canWalkThroughWalls = false;
     private bool canMoveBombs = false;
     private bool hasShield = false;
     private bool canCreateWall = false;
+    private bool stunned = false;
 
     private bool placeBomb;
     public GameObject bomb;
@@ -35,41 +36,62 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKeyUp("space"))
+        if (!stunned)
         {
-            placeBomb = true;
+            if (Input.GetKeyUp("space"))
+            {
+                placeBomb = true;
+            }
         }
     }
 
     private void FixedUpdate()
     {
-        // Movement
-        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
 
-        if (placeBomb)
+        if (!stunned)
         {
-            GameObject newBomb = Instantiate(bomb, new Vector2(Mathf.RoundToInt(rb.position.x),
-                                            Mathf.RoundToInt(rb.position.y)), transform.rotation);
-            Destroy(newBomb, 2);
-            placeBomb = false;
-   
+            // Movement
+            rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+
+            if (placeBomb)
+            {
+                GameObject newBomb = Instantiate(bomb, new Vector2(Mathf.RoundToInt(rb.position.x),
+                                                Mathf.RoundToInt(rb.position.y)), transform.rotation);
+                Destroy(newBomb, 2);
+                placeBomb = false;
+
+            }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if ( collision.gameObject.tag == "Movement")
+        if ( collision.gameObject.tag == "Movement" )
         {
             // increase speed for movement buff
             speed += 3.5f;
             Destroy(collision.gameObject);
 
-        } else if ( collision.gameObject.tag == "MovementDebuff")
+        } else if ( collision.gameObject.tag == "MovementDebuff" )
         {
             // decrease speed for movement debuff
             speed -= 3.5f;
             Destroy(collision.gameObject);
 
+        } else if ( collision.gameObject.tag == "SelfStun" )
+        {
+
+            // momentarily stop movement and ability to place bombs
+            StartCoroutine(performSelfStun());
+            Destroy(collision.gameObject);
+
         }
+    }
+
+    IEnumerator performSelfStun()
+    {
+        stunned = true;
+        yield return new WaitForSeconds(2.0f);
+        stunned = false;
     }
 }
