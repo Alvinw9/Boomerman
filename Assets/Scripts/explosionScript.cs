f4cf8b26ed714ede5fd004fcb91f74666427baa7
Alvinw9 <33185325+Alvinw9@users.noreply.github.com>
2020-02-24 20:00:05 -0800

    bomb destroys player

---
+++ Assets/Scripts/explosionScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosionScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
